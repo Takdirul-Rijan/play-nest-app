2019294196412da2c3ca6fad2d7c4fce6685b205
Takdirul-Rijan <takdirul.rijan@gmail.com>
2025-10-24 20:30:39 +0600

    Create interactive Swiper slider section

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,42 +1,34 @@
 import React, { use } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
-const sliderPromise = fetch("/toydata.json").then((res) => res.json());
+const sliderPromise = fetch("/slider.json").then((res) => res.json());
 
 const Slider = () => {
-  // console.log(sliderPromise);
-
   const slider = use(sliderPromise);
 
   return (
-    <section className="w-full py-3 bg-gradient-to-r from-pink-50 via-rose-50 to-yellow-50">
-      <div className="w-11/12 mx-auto relative">
-        <div className="carousel w-full">
+    <section className="w-full h-[80vh] md:h-[90vh] bg-gradient-to-r from-pink-50 via-rose-50 to-yellow-50">
+      <div className="w-full h-full relative">
+        <Swiper
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper w-full h-full"
+        >
           {slider.map((slide) => (
-            <div
-              key={slide.toyId}
-              id={`slide${slide.toyId}`}
-              className="carousel-item relative w-full"
-            >
-              <img
-                src={slide.pictureURL}
-                className="w-full h-64 md:h-80 object-contain"
-                alt=""
-              />
-            </div>
+            <SwiperSlide key={slide.toyId}>
+              <div className="w-full h-full relative">
+                <img
+                  src={slide.pictureURL}
+                  alt=""
+                  className="w-full h-full object-center"
+                />
+              </div>
+            </SwiperSlide>
           ))}
-        </div>
-
-        <div className="flex justify-center gap-2 mt-4">
-          {slider.map((slide) => (
-            <a
-              key={slide.toyId}
-              href={`#slide${slide.toyId}`}
-              className="btn btn-xm hover:bg-green-400"
-            >
-              •
-            </a>
-          ))}
-        </div>
+        </Swiper>
       </div>
     </section>
   );
